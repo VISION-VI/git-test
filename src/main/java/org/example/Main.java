7921e1cf5ae442870736b41f8502ca9680f9392b
Linhe <Linhe@Linhe.com>
2025-02-23 17:41:00 +0800

    push commit3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("push test");
         System.out.println("push test2");
         System.out.println("pull test");
+        System.out.println("push test3");
     }
 }
